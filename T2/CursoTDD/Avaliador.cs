96f72b1f232c3f6e20b7a7c5203cd201199f2dac
m010 <m010@benner.com.br>
2018-08-13 11:34:19 -0300

    Dale

---
+++ T2/CursoTDD/Avaliador.cs
@@ -0,0 +1,33 @@
+﻿using Caelum.Leilao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoTDD
+{
+    public class Avaliador
+    {
+        private double maiorTodos = Double.MinValue;
+        private double menorTodos = Double.MaxValue;
+
+        public void Avalia(Leilao leilao)
+        {
+            foreach (var lance in leilao.Lances)
+            {
+                if (lance.Valor > maiorTodos)
+                {
+                    maiorTodos = lance.Valor;
+                }
+                if (lance.Valor < menorTodos)
+                {
+                    menorTodos = lance.Valor;
+                }
+            }
+        }
+
+        public double MaiorLance { get { return maiorTodos; } }
+        public double MenorLance { get { return menorTodos; } }
+    }
+}
---
+++ T2/CursoTDD/AvaliadorTest.cs
@@ -0,0 +1,38 @@
+﻿using Caelum.Leilao;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoTDD
+{
+    [TestFixture]
+    class AvaliadorTest
+    {
+        [Test]
+        public void DeveEntenderLancesEmOrdemCrescente()
+        {
+            Usuario joao = new Usuario("Joao");
+            Usuario jose = new Usuario("José");
+            Usuario maria = new Usuario("Maria");
+
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            leilao.Propoe(new Lance(maria, 250.0));
+            leilao.Propoe(new Lance(joao, 300.0));
+            leilao.Propoe(new Lance(jose, 400.0));
+
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            double maiorEsperado = 400;
+            double menorEsperado = 250;
+
+            Console.WriteLine(maiorEsperado == leiloeiro.MaiorLance);
+            Console.WriteLine(menorEsperado == leiloeiro.MenorLance);
+        }
+    }
+
+}
